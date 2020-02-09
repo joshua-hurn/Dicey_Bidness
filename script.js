ba0c96af1dfb39b8da5c27507fbc312ce61313d1
Josh Hurn <42974930+joshua-hurn@users.noreply.github.com>
2020-02-09 15:11:18 -0600

    added dice delete

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const container = document.getElementById('die-container'),
     generateButton = document.getElementById('generate-die'),
-    rollButton = document.getElementById('roll-die');
+    rollButton = document.getElementById('roll-die'),
+    sumButton = document.getElementById('sum-die');
 let numOfDice = 0,
     diceArr = [];
 
+// Create a new dice object.
 generateButton.addEventListener('click', () => {
     new Die();
 });
 
+// Roll the dice.
 rollButton.addEventListener('click', () => {
     diceArr.forEach(die => {
-        console.log(die);
         die.roll();
     });
+});
+
+// Sum the dice.
+sumButton.addEventListener('click', () => {
+    let sum = 0;
+    diceArr.forEach(die => {
+        sum = sum + die.value
+    });
+    alert(sum);
 });
 
 class Die {
@@ -27,6 +38,10 @@ class Die {
         diceArr.push(this); // do not push this.div. try changing to this to this.div and see what happens!
         this.div.addEventListener('click', () => {
             this.roll();
+        });
+        this.div.addEventListener('dblclick', () => {
+            container.removeChild(this.div);
+            diceArr.pop(this.div);
         });
     }
 
